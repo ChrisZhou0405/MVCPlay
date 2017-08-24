7b3ad99b9be587cee783a695847389f998233a6f
christ.zhou <christ.zhou@ZU-PC46>
2017-08-24 18:46:41 +0800

    特性标签

---
+++ 高级语法/Cat.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 高级语法
+{
+    [Vip][Obsolete]
+    class Cat
+    {
+
+        public string Name { get; set; }
+        [Vip]
+        public int Age { get; set; }
+        public string Eat()
+        {
+            return "普通的吃";
+        }
+
+        public string EatNice()
+        {
+            return "优雅的吃";
+        }
+    }
+}
---
+++ 高级语法/VipAttribute.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 高级语法
+{
+    [AttributeUsage(AttributeTargets.Class|AttributeTargets.Property|AttributeTargets.Field|AttributeTargets.Method,AllowMultiple =true,Inherited =false)]
+  public  class VipAttribute:Attribute
+    {
+    }
+}
